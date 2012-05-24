9b6cd725ccf8bc05c1684389c073485a411f5a52
Alexey Zakharenko <yogin.alex@gmail.com>
2012-05-24 14:51:47 +0400

    Первая заливка

---
+++ urls.py
@@ -0,0 +1,10 @@
+# This also imports the include function
+from django.conf.urls.defaults import *
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
